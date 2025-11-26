d929986365cb8c3bc3144eee317529f1c6a28d10
Michel255-dev <ngoranjeanmichelvianney@gmail.com>
2025-11-26 18:19:28 +0000

    Fix vite pour le deployement

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-module.exports = {
-  darkMode:'class',
-  content : [
-    './src/**/*.{js,jsx,ts,tsx},'
-  ],
-  theme:{},
-}
 
 // https://vite.dev/config/
 export default defineConfig({
